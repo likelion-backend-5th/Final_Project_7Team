41fe3620d8497a146b3d2fe7ade18a4e7d8bd5d3
2hapu <yonzn@naver.com>
2023-08-16 13:32:29 +0900

    first commit

---
+++ src/main/java/com/likelion/catdogpia/CatdogpiaApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.catdogpia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatdogpiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatdogpiaApplication.class, args);
+	}
+
+}
