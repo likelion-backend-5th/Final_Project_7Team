18cf387692722414fc5ec70efc472b730106f5e4
kkyuwoo <ggya5@naver.com>
2023-08-16 23:08:51 +0900

    feat: AttachEntity 생성

---
+++ src/main/java/com/likelion/catdogpia/domain/entity/attach/Attach.java
@@ -0,0 +1,30 @@
+package com.likelion.catdogpia.domain.entity.attach;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Attach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attach_id")
+    private Long attachId;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Attach(Long attachId, LocalDateTime createdAt) {
+        this.attachId = attachId;
+        this.createdAt = createdAt;
+    }
+}
