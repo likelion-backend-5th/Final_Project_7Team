c24356e6a371a7c1ddb964c7f64e3a164e2280c9
yongjunlee <93720125+2hapu@users.noreply.github.com>
2023-08-17 11:04:19 +0900

    #2 주문, 리뷰 엔티티 생성, 베이스 엔티티 수정 (#23)
    * refactor: BaseEntity 수정
    
    * refactor: Orders, Review Entity 생성
    
    * feat: 주문, 리뷰 Entity 생성
    
    * refactor: SuperBuilder toBuilder 삭제
    
    * refactor: pk column name 삭제
    
    * refactor: builder 어노테이션 수정

---
+++ src/main/java/com/likelion/catdogpia/domain/entity/order/Orders.java
@@ -0,0 +1,61 @@
+package com.likelion.catdogpia.domain.entity.order;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name ="orders")
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    // 회원
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 20)
+    private String phone;
+
+    @Column(nullable = false, length = 150)
+    private String address;
+
+    @Column(length = 150)
+    private String request;
+
+    @Column(length = 30)
+    private String cardCompany;
+
+    private int totalAmount;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime orderAt;
+
+    private LocalDateTime cancelAt;
+
+    @Builder
+    public Orders(Long id, String name, String phone, String address, String request, String cardCompany,
+                  int totalAmount, LocalDateTime orderAt, LocalDateTime cancelAt) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.request = request;
+        this.cardCompany = cardCompany;
+        this.totalAmount = totalAmount;
+        this.orderAt = orderAt;
+        this.cancelAt = cancelAt;
+    }
+}
---
+++ src/main/java/com/likelion/catdogpia/domain/entity/review/Review.java
@@ -0,0 +1,45 @@
+package com.likelion.catdogpia.domain.entity.review;
+
+import com.likelion.catdogpia.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+@Table(name ="review")
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //  회원
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+    // 주문 상품
+//    @OneToOne
+//    @JoinColumn(name = "order_product_id")
+//    private OrderProduct orderProduct;
+
+    // 파일
+//    OneToOne
+//    @JoinColumn(name = "attach_id")
+//    private Attach attach;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Builder
+    public Review(Long id, String description, int rating) {
+        this.id = id;
+        this.description = description;
+        this.rating = rating;
+    }
+}
