b875948d96327e75567a7fea0a0189e1447f6e1e
kkyuwoo <ggya5@naver.com>
2023-08-17 13:10:11 +0900

    feat: CommentEntity 연관관계 매핑

--- src/main/java/com/likelion/catdogpia/domain/entity/community/Comment.java
+++ src/main/java/com/likelion/catdogpia/domain/entity/community/Comment.java
@@ -1,12 +1,16 @@
 package com.likelion.catdogpia.domain.entity.community;
 
 import com.likelion.catdogpia.domain.entity.BaseEntity;
+import com.likelion.catdogpia.domain.entity.report.Report;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -19,6 +23,15 @@ public class Comment extends BaseEntity {
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
+
+    //커뮤니티 글 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    //신고 연관관계
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
+    private List<Report> reportList = new ArrayList<>();
 
     @Builder
     public Comment(Long id, String content) {
