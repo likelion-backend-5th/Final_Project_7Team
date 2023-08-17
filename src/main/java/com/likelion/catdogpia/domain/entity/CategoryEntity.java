d4d0b43d7d6f0a7de2ca8604f22edb3c5a323494
kkyuwoo <ggya5@naver.com>
2023-08-17 11:48:02 +0900

    feat: CategoryEntity 연관관계 매핑

--- src/main/java/com/likelion/catdogpia/domain/entity/CategoryEntity.java
+++ src/main/java/com/likelion/catdogpia/domain/entity/CategoryEntity.java
@@ -1,10 +1,15 @@
 package com.likelion.catdogpia.domain.entity;
 
+import com.likelion.catdogpia.domain.entity.community.Article;
+import com.likelion.catdogpia.domain.entity.product.Product;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -19,6 +24,22 @@ public class CategoryEntity {
 
     @Column(length = 1, nullable = false)
     private Character useYn;
+
+    //카테고리(대분류-중분류) 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private CategoryEntity parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL)
+    private List<CategoryEntity> categoryEntityList = new ArrayList<>();
+
+    //상품 연관관계
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> productList = new ArrayList<>();
+
+    //커뮤니티 연관관계
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Article> articleList = new ArrayList<>();
 
     @Builder
     public CategoryEntity(Long id, String name, Character useYn) {
