a75cbcfd6a5ca4195131b28f26c8a9e737397efd
kkyuwoo <ggya5@naver.com>
2023-08-17 13:10:02 +0900

    feat: ArticleEntity 연관관계 매핑

--- src/main/java/com/likelion/catdogpia/domain/entity/community/Article.java
+++ src/main/java/com/likelion/catdogpia/domain/entity/community/Article.java
@@ -1,12 +1,19 @@
 package com.likelion.catdogpia.domain.entity.community;
 
 import com.likelion.catdogpia.domain.entity.BaseEntity;
+import com.likelion.catdogpia.domain.entity.CategoryEntity;
+import com.likelion.catdogpia.domain.entity.attach.Attach;
+import com.likelion.catdogpia.domain.entity.report.Report;
+import com.likelion.catdogpia.domain.entity.user.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -28,6 +35,29 @@ public class Article extends BaseEntity {
 
     @Column(nullable = false)
     private Integer likeCnt;
+
+    //카테고리 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
+
+    //댓글 연관관계
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private List<Comment> commentList = new ArrayList<>();
+
+    //파일 연관관계
+    @OneToOne
+    @JoinColumn(name = "article_id")
+    private Attach attach;
+
+    //회원 연관관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    //신고 연관관계
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private List<Report> reportList = new ArrayList<>();
 
     @Builder
     public Article(Long id, String title, String content, Integer viewCnt, Integer likeCnt) {
