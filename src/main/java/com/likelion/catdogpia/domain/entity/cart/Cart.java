957fb726336cea018536e5befd0a169665ce6455
choiseonyoung <44860284+choiseonyoung@users.noreply.github.com>
2023-08-17 11:08:40 +0900

    feat: 장바구니 엔티티 생성
    * feat : CartEntity 생성
    
    * refactor : CartEntity 패키지 위치 변경
    
    * refactor : CartEntity 수정
    
    * refactor: CartEntity 수정
    
    * refactor: CartEntity 수정

---
+++ src/main/java/com/likelion/catdogpia/domain/entity/cart/Cart.java
@@ -0,0 +1,33 @@
+package com.likelion.catdogpia.domain.entity.cart;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 상품 개수
+    @Column(nullable = false)
+    private Integer productCnt;
+
+    @Builder
+    public Cart(Long id, Integer productCnt) {
+        this.id = id;
+        this.productCnt = productCnt;
+    }
+
+    // 회원
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Member member;
+
+    // 상품 옵션
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private ProductOption productOption;
+
+}
