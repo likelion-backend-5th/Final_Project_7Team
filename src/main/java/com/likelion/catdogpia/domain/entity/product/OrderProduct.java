266155e3eae42cda5c647de4a3dfed49c7abfc6e
yongjunlee <93720125+2hapu@users.noreply.github.com>
2023-08-17 11:03:59 +0900

    #24 상품 관련 엔티티 생성
    * feat: 상품 관련 Entity 생성
    
    * refactor: @AllArgsConstructor 삭제
    
    * refactor: pk name 수정
    
    * refactor: builder 어노테이션 수정
    
    * refactor: fetchType 수정

---
+++ src/main/java/com/likelion/catdogpia/domain/entity/product/OrderProduct.java
@@ -0,0 +1,46 @@
+package com.likelion.catdogpia.domain.entity.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name ="order_product")
+public class OrderProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 주문
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "order_id")
+//    private Orders order;
+
+    // 상품 옵션
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_id")
+    private ProductOption productOption;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(length = 20, nullable = false)
+    private String orderStatus;
+
+    private LocalDateTime receivedAt;
+
+    private LocalDateTime shippedAt;
+
+
+    @Builder
+    public OrderProduct(Long id, ProductOption productOption, int quantity, String orderStatus, LocalDateTime receivedAt, LocalDateTime shippedAt) {
+        this.id = id;
+        this.productOption = productOption;
+        this.quantity = quantity;
+        this.orderStatus = orderStatus;
+        this.receivedAt = receivedAt;
+        this.shippedAt = shippedAt;
+    }
+}
