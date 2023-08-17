fc16cf7c9d7b143bd5f1a0726624a7dd15708163
yongjunlee <93720125+2hapu@users.noreply.github.com>
2023-08-17 11:35:12 +0900

    feat: 상품 관련 엔티티 추가
    * feat: 상품 관련 Entity 생성
    
    * refactor: @AllArgsConstructor 삭제
    
    * refactor: pk name 수정
    
    * refactor: builder 어노테이션 수정
    
    * refactor: fetchType 수정
    
    * feat: 상품 관련 엔티티 추가

---
+++ src/main/java/com/likelion/catdogpia/domain/entity/product/Product.java
@@ -0,0 +1,48 @@
+package com.likelion.catdogpia.domain.entity.product;
+
+import com.likelion.catdogpia.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+@Table(name ="product")
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 카테고리1
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name ="category_id")
+//    private Categoty categoty;
+
+    // 파일
+//    @OneToOne
+//    @JoinColumn(name = "attach_id")
+//    private Attach attach;
+
+    @Column(length = 40, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(length = 10, nullable = false)
+    private String status;
+
+    @Builder
+    public Product(Long id, String name, int price, String content, String status) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.content = content;
+        this.status = status;
+    }
+}
