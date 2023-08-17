738c82050e061f144d66c9a454fcbfc0b4574248
kkyuwoo <ggya5@naver.com>
2023-08-17 13:21:11 +0900

    feat: AttachEntity 연관관계 매핑

--- src/main/java/com/likelion/catdogpia/domain/entity/attach/Attach.java
+++ src/main/java/com/likelion/catdogpia/domain/entity/attach/Attach.java
@@ -1,5 +1,8 @@
 package com.likelion.catdogpia.domain.entity.attach;
 
+import com.likelion.catdogpia.domain.entity.community.Article;
+import com.likelion.catdogpia.domain.entity.product.Product;
+import com.likelion.catdogpia.domain.entity.review.Review;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -8,6 +11,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -20,6 +25,25 @@ public class Attach {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    //파일상세 연관관계
+    @OneToMany(mappedBy = "attach", cascade = CascadeType.ALL)
+    private List<AttachDetail> attachDetailList = new ArrayList<>();
+
+    //상품 연관관계
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    //커뮤니티 글 연관관계
+    @OneToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    //리뷰 연관관계
+    @OneToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
 
     @Builder
     public Attach(Long id, LocalDateTime createdAt) {
