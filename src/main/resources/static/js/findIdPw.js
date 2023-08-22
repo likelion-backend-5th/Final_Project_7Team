6abc5aa872bcfe8e7c47439f208828dbf5ddec86
kkyuwoo <ggya5@naver.com>
2023-08-23 00:12:08 +0900

    feat: 아이디 찾기, 비밀번호 찾기 Front-Back 연결

--- src/main/resources/static/js/findIdPw.js
+++ src/main/resources/static/js/findIdPw.js
@@ -35,3 +35,40 @@ function findId() {
             console.error("Error finding ID", error);
         });
 }
+
+//비밀번호 찾기(계정 있는지 검사)
+function findPw() {
+    const loginId = document.getElementById("loginId").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    fetch(`/findPassword?loginId=${loginId}&name=${name}&email=${email}`)
+        .then(response => response.json())
+        .then(data => {
+            openModal(data.message);
+            if (data.message !== "입력하신 이메일로 임시 비밀번호를 발급합니다.") {
+                document.getElementById("getPwBtn").style.display = "none";
+            } else {
+                document.getElementById("getPwBtn").style.display = "block";
+            }
+        })
+        .catch(error => {
+            console.error("Error finding Password", error);
+        });
+}
+
+//비밀번호 찾기(임시 비밀번호 전송)
+function getTempPw() {
+    $.ajax({
+        url: "/tempPassword",
+        type: "post",
+        dataType: "text",
+        data: { "email": $("#email").val() },
+        success: function (data) {
+            alert("임시 비밀번호가 전송되었습니다.");
+        },
+        error: function (request, status, error) {
+            alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+        }
+    });
+}
